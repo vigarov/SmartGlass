4614c4d28cbb6d304a399e73103b826dce22d4db
Victor G <vesbru@yahoo.fr>
2022-06-02 17:16:07 +0200

    notifications images refactor, notification container, updatable system time on header

---
+++ src/firmware/esp_idf_glasses/main/Display/Objects/Mutable/NotificationContainer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Display.h"
+#include "Notification.h"
+#include "Container.h"
+#include "Logos.h"
+#include <memory>
+#include <string>
+
+namespace SmartGlasses{
+
+    class NotificationContainer : public Container{
+        #define NOTIF_LOGO_OFFSET_Y 5
+        #define NOTIF_TITLE_OFFSET_X 25
+        #define NOTIF_TITLE_OFFSET_Y 7
+        #define NOTIF_ADDINFO_OFFSET_X NOTIF_TITLE_OFFSET_X
+        #define NOTIF_ADDINFO_OFFSET_Y ((NOTIF_TITLE_OFFSET_Y)+10)
+    public:
+        NotificationContainer(std::string& s,notification_t notifcation,bool overwrites = true, pixel_pair_t offsets = {0,0}, unsigned char animate = false, unsigned char priority = 2,TaskHandle_t notifyOnDraw = nullptr);
+    private:
+        notification_t m_notification;
+
+        std::shared_ptr<Logo> getNotificationLogo(app_t n, pixel_pair_t offsets);
+        std::string parseTerminated(char* s, bool terminated);
+    };
+
+};
